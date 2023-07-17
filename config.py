c536506b85811d4a88b8c597c0fea408aa260993
karam <karamissa987@gmail.com>
2023-07-17 14:27:12 +0300

    config file for bothe consumer and producer

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import asyncio
 # env Variable
 
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9093"
-KAFKA_TOPIC = "postTopic"
-KAFKA_CONSUMER_GROUP = "postMicroservice"
+KAFKA_TOPIC_POST = "postTopic"
+KAFKA_CONSUMER_GROUP_POST = "postMicroservice"
+KAFKA_TOPIC_USER = "userStatsTopic"
+KAFKA_CONSUMER_GROUP_USER = "userMicroservice"
 
 loop = asyncio.get_event_loop()
